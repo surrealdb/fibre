be9c09779766085884f9c64457c7adc02381eef7
Tobie Morgan Hitchcock <tobie@abcum.com>
2016-09-15 09:43:48 +0100

    Add improved middleware checks

--- middle.go
+++ middle.go
@@ -16,61 +16,235 @@ package fibre
 
 import (
 	"regexp"
+	"strings"
 )
 
-func check(what string, checks ...string) bool {
-	for _, check := range checks {
-		if regex, err := regexp.Compile(check); err == nil {
-			if regex.Match([]byte(what)) {
-				return true
-			}
-		}
-	}
-	return false
-}
+// ----------------------------------------------------------------------------------------------------
 
-// Host checks if the url host matches in the request. If the host matches the middleware will be invoked.
-func (m MiddlewareFunc) Host(test ...string) MiddlewareFunc {
+// HostIs checks if the url path matches in the request. If the path matches the middleware will be invoked.
+func (m MiddlewareFunc) HostIs(tests ...string) MiddlewareFunc {
 	return func(h HandlerFunc) HandlerFunc {
 		return func(c *Context) error {
-			if check(c.Request().URL().Host, test...) {
-				return m(h)(c)
+			for _, test := range tests {
+				if c.Request().URL().Host == test {
+					return m(h)(c)
+				}
 			}
 			return h(c)
 		}
 	}
 }
 
-// Path checks if the url path matches in the request. If the path matches the middleware will be invoked.
-func (m MiddlewareFunc) Path(test ...string) MiddlewareFunc {
+// HostMatches checks if the url path matches in the request. If the path matches the middleware will be invoked.
+func (m MiddlewareFunc) HostMatches(tests ...regexp.Regexp) MiddlewareFunc {
 	return func(h HandlerFunc) HandlerFunc {
 		return func(c *Context) error {
-			if check(c.Request().URL().Path, test...) {
-				return m(h)(c)
+			for _, test := range tests {
+				if test.MatchString(c.Request().URL().Host) {
+					return m(h)(c)
+				}
 			}
 			return h(c)
 		}
 	}
 }
 
-// Scheme checks if the url scheme matches in the request. If the scheme matches the middleware will be invoked.
-func (m MiddlewareFunc) Scheme(test ...string) MiddlewareFunc {
+// HostBegsWith checks if the url path matches in the request. If the path matches the middleware will be invoked.
+func (m MiddlewareFunc) HostBegsWith(tests ...string) MiddlewareFunc {
 	return func(h HandlerFunc) HandlerFunc {
 		return func(c *Context) error {
-			if check(c.Request().URL().Scheme, test...) {
-				return m(h)(c)
+			for _, test := range tests {
+				if strings.HasPrefix(c.Request().URL().Host, test) {
+					return m(h)(c)
+				}
 			}
 			return h(c)
 		}
 	}
 }
 
-// UserAgent checks if the user agent matches in the request. If the user agent matches the middleware will be invoked.
-func (m MiddlewareFunc) UserAgent(test ...string) MiddlewareFunc {
+// HostEndsWith checks if the url path matches in the request. If the path matches the middleware will be invoked.
+func (m MiddlewareFunc) HostEndsWith(tests ...string) MiddlewareFunc {
 	return func(h HandlerFunc) HandlerFunc {
 		return func(c *Context) error {
-			if check(c.Request().UserAgent(), test...) {
-				return m(h)(c)
+			for _, test := range tests {
+				if strings.HasSuffix(c.Request().URL().Host, test) {
+					return m(h)(c)
+				}
+			}
+			return h(c)
+		}
+	}
+}
+
+// ----------------------------------------------------------------------------------------------------
+
+// PathIs checks if the url path matches in the request. If the path matches the middleware will be invoked.
+func (m MiddlewareFunc) PathIs(tests ...string) MiddlewareFunc {
+	return func(h HandlerFunc) HandlerFunc {
+		return func(c *Context) error {
+			for _, test := range tests {
+				if c.Request().URL().Path == test {
+					return m(h)(c)
+				}
+			}
+			return h(c)
+		}
+	}
+}
+
+// PathMatches checks if the url path matches in the request. If the path matches the middleware will be invoked.
+func (m MiddlewareFunc) PathMatches(tests ...regexp.Regexp) MiddlewareFunc {
+	return func(h HandlerFunc) HandlerFunc {
+		return func(c *Context) error {
+			for _, test := range tests {
+				if test.MatchString(c.Request().URL().Path) {
+					return m(h)(c)
+				}
+			}
+			return h(c)
+		}
+	}
+}
+
+// PathBegsWith checks if the url path matches in the request. If the path matches the middleware will be invoked.
+func (m MiddlewareFunc) PathBegsWith(tests ...string) MiddlewareFunc {
+	return func(h HandlerFunc) HandlerFunc {
+		return func(c *Context) error {
+			for _, test := range tests {
+				if strings.HasPrefix(c.Request().URL().Path, test) {
+					return m(h)(c)
+				}
+			}
+			return h(c)
+		}
+	}
+}
+
+// PathEndsWith checks if the url path matches in the request. If the path matches the middleware will be invoked.
+func (m MiddlewareFunc) PathEndsWith(tests ...string) MiddlewareFunc {
+	return func(h HandlerFunc) HandlerFunc {
+		return func(c *Context) error {
+			for _, test := range tests {
+				if strings.HasSuffix(c.Request().URL().Path, test) {
+					return m(h)(c)
+				}
+			}
+			return h(c)
+		}
+	}
+}
+
+// ----------------------------------------------------------------------------------------------------
+
+// SchemeIs checks if the url path matches in the request. If the path matches the middleware will be invoked.
+func (m MiddlewareFunc) SchemeIs(tests ...string) MiddlewareFunc {
+	return func(h HandlerFunc) HandlerFunc {
+		return func(c *Context) error {
+			for _, test := range tests {
+				if c.Request().URL().Scheme == test {
+					return m(h)(c)
+				}
+			}
+			return h(c)
+		}
+	}
+}
+
+// SchemeMatches checks if the url path matches in the request. If the path matches the middleware will be invoked.
+func (m MiddlewareFunc) SchemeMatches(tests ...regexp.Regexp) MiddlewareFunc {
+	return func(h HandlerFunc) HandlerFunc {
+		return func(c *Context) error {
+			for _, test := range tests {
+				if test.MatchString(c.Request().URL().Scheme) {
+					return m(h)(c)
+				}
+			}
+			return h(c)
+		}
+	}
+}
+
+// SchemeBegsWith checks if the url path matches in the request. If the path matches the middleware will be invoked.
+func (m MiddlewareFunc) SchemeBegsWith(tests ...string) MiddlewareFunc {
+	return func(h HandlerFunc) HandlerFunc {
+		return func(c *Context) error {
+			for _, test := range tests {
+				if strings.HasPrefix(c.Request().URL().Scheme, test) {
+					return m(h)(c)
+				}
+			}
+			return h(c)
+		}
+	}
+}
+
+// SchemeEndsWith checks if the url path matches in the request. If the path matches the middleware will be invoked.
+func (m MiddlewareFunc) SchemeEndsWith(tests ...string) MiddlewareFunc {
+	return func(h HandlerFunc) HandlerFunc {
+		return func(c *Context) error {
+			for _, test := range tests {
+				if strings.HasSuffix(c.Request().URL().Scheme, test) {
+					return m(h)(c)
+				}
+			}
+			return h(c)
+		}
+	}
+}
+
+// ----------------------------------------------------------------------------------------------------
+
+// AgentIs checks if the url path matches in the request. If the path matches the middleware will be invoked.
+func (m MiddlewareFunc) AgentIs(tests ...string) MiddlewareFunc {
+	return func(h HandlerFunc) HandlerFunc {
+		return func(c *Context) error {
+			for _, test := range tests {
+				if c.Request().UserAgent() == test {
+					return m(h)(c)
+				}
+			}
+			return h(c)
+		}
+	}
+}
+
+// AgentMatches checks if the url path matches in the request. If the path matches the middleware will be invoked.
+func (m MiddlewareFunc) AgentMatches(tests ...regexp.Regexp) MiddlewareFunc {
+	return func(h HandlerFunc) HandlerFunc {
+		return func(c *Context) error {
+			for _, test := range tests {
+				if test.MatchString(c.Request().UserAgent()) {
+					return m(h)(c)
+				}
+			}
+			return h(c)
+		}
+	}
+}
+
+// AgentBegsWith checks if the url path matches in the request. If the path matches the middleware will be invoked.
+func (m MiddlewareFunc) AgentBegsWith(tests ...string) MiddlewareFunc {
+	return func(h HandlerFunc) HandlerFunc {
+		return func(c *Context) error {
+			for _, test := range tests {
+				if strings.HasPrefix(c.Request().UserAgent(), test) {
+					return m(h)(c)
+				}
+			}
+			return h(c)
+		}
+	}
+}
+
+// AgentEndsWith checks if the url path matches in the request. If the path matches the middleware will be invoked.
+func (m MiddlewareFunc) AgentEndsWith(tests ...string) MiddlewareFunc {
+	return func(h HandlerFunc) HandlerFunc {
+		return func(c *Context) error {
+			for _, test := range tests {
+				if strings.HasSuffix(c.Request().UserAgent(), test) {
+					return m(h)(c)
+				}
 			}
 			return h(c)
 		}
