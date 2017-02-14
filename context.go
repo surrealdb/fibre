06253aa0e81ee7c1cf35d7f4744998af89ffb66d
Tobie Morgan Hitchcock <tobie@abcum.com>
2017-02-14 19:11:38 +0000

    Increase default websocket read/write buffer sizes

--- context.go
+++ context.go
@@ -33,8 +33,8 @@ import (
 )
 
 var wsupgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
+	ReadBufferSize:    4096,
+	WriteBufferSize:   4096,
 	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
