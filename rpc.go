4daebfda4241ab8996127e91f52cb674a7919a63
Tobie Morgan Hitchcock <tobie@abcum.com>
2017-11-14 01:37:08 +0000

    Add RPCNotification type for jsonrpc notifications

--- rpc.go
+++ rpc.go
@@ -31,9 +31,9 @@ type RPCError struct {
 // RPCRequest represents an incoming jsonrpc request
 type RPCRequest struct {
 	ID     interface{}   `json:"id" msgpack:"id"`
-	Async  bool          `json:"async" msgpack:"async"`
-	Method string        `json:"method" msgpack:"method"`
-	Params []interface{} `json:"params" msgpack:"params"`
+	Async  bool          `json:"async,omitempty" msgpack:"async,omitempty"`
+	Method string        `json:"method,omitempty" msgpack:"method,omitempty"`
+	Params []interface{} `json:"params,omitempty" msgpack:"params,omitempty"`
 }
 
 // RPCResponse represents an outgoing jsonrpc response
@@ -41,6 +41,13 @@ type RPCResponse struct {
 	ID     interface{} `json:"id" msgpack:"id"`
 	Error  *RPCError   `json:"error,omitempty" msgpack:"error,omitempty"`
 	Result interface{} `json:"result,omitempty" msgpack:"result,omitempty"`
+}
+
+// RPCNotification represents an outgoing jsonrpc notification
+type RPCNotification struct {
+	ID     interface{}   `json:"id" msgpack:"id"`
+	Method string        `json:"method,omitempty" msgpack:"method,omitempty"`
+	Params []interface{} `json:"params,omitempty" msgpack:"params,omitempty"`
 }
 
 // Rpc adds a route > handler to the router for a jsonrpc endpoint.
